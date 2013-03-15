cae7bb9e9af08ba24b56bc82e68ce1f2640a3245
Alex Kiesel <alex@kiesel.name>
2013-03-15 06:48:49 +0100

    - support different commands

--- host/bin/sublime-net-invoke.sh
+++ host/bin/sublime-net-invoke.sh
@@ -8,17 +8,38 @@ TRNS="A:/"
 
 while [ true ]; do
   echo "Listening ...";
-  nc -l 127.0.0.1 5115 | while read -r fn ; do
-    echo ">>> $fn";
+  nc -l 127.0.0.1 5115 | while read -r -a 'RARG' ; do
+    echo ">>> $RARG";
+    echo "  > $RARG[0]"
 
-    # Now translate to local filesystem path ...
-    echo $BASE
-    FILES=$(echo "$fn" | sed -r "s|$BASE|$TRNS|g")
+    case $RARG[1] in
+      EDIT)
+        # Now translate to local filesystem path ...
+        FILES=$(echo "$fn" | sed -r "s|$BASE|$TRNS|g")
 
-    echo "   > $FILES"
+        echo "   > $FILES"
 
-    # Invoke Sublime
-    '/cygdrive/c/Program Files/Sublime Text 2/sublime_text.exe' "$FILES" &
+        # Invoke Sublime
+        # '/cygdrive/c/Program Files/Sublime Text 2/sublime_text.exe' "$FILES" & ;
+        ;;
+
+      SHELL)
+        # TODO
+        DIR=FILES=$(echo "$fn" | sed -r "s|$BASE|$TRNS|g")
+        echo "Opening shell at $DIR"
+        ;;
+
+      EXPLORE)
+        # TODO
+        DIR=FILES=$(echo "$fn" | sed -r "s|$BASE|$TRNS|g")
+        echo "Opening explorer at $DIR"
+        ;;
+
+      *)
+        echo "Unknown command $RARG[1]";
+        ;;
+    esac
+
     echo
   done
 done
