56bac21c763c171227ead699c74f1b0f0d68e401
Alex Kiesel <alex.kiesel@1und1.de>
2014-07-23 22:57:02 +0200

    Add installer for host, guest

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e -u 
+
+MODE=${1:-}
+TARGET=$HOME/bin
+
+case $MODE in
+  "guest")
+    echo "===> Installing wormhole guest into $TARGET"
+    for i in s expl term; do
+      echo "   > $i"
+      ln -sf $(realpath ./guest/bin/$i) $TARGET/$i
+    done
+    ;;
+
+  "host")
+    echo "===> Installing wormhole guest into $TARGET"
+    for i in net-invoke.sh; do
+      echo "   > $i"
+      ln -sf $(realpath ./guest/bin/$i) $TARGET/$i
+    done
+    ;;
+
+  *)
+    echo "$0 host|guest"
+    echo "  host  -> installs host side"
+    echo "  guest -> installs guest side"
+    ;;
+esac 
