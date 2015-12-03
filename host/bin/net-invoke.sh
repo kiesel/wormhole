509c451409a1b4d5f4b42387f69a39d1bf508f7a
Alex D'Andrea <alex.dandrea@1und1.de>
2015-12-03 20:54:57 +0100

    Refactor to increase readability

--- host/bin/net-invoke.sh
+++ host/bin/net-invoke.sh
@@ -2,7 +2,8 @@
 set -e 
 set -u 
 
-PIDFILE="/var/run/$(basename $0).pid"
+PIDFILE="/var/run/net-invoke.pid"
+LOCK_FD=9 # arbitrary file descriptor
 
 ## Translation settings
 # BASE - translate this:
@@ -17,93 +18,116 @@ PORT=${WORMHOLE_PORT:-5115}
 NET_VISUAL=${WORMHOLE_EDITOR:-'/cygdrive/c/Program Files/Sublime Text 3/sublime_text.exe'}
 NET_TERM=${WORMHOLE_TERMINAL:-mintty}
 
-if [ -r $PIDFILE ]; then
-  echo "$0 already running. Quitting.";
-  exit;
-fi
+lock() {
+  local LOCKFILE=$1
+  local FD=${2:-$LOCK_FD}
 
-# Ensure PID file is removed on program exit.
-trap "rm -f -- '$PIDFILE'" EXIT
+  # Create lockfile
+  eval "exec ${FD}>${LOCKFILE}"
 
-# Write pidfile
-echo $$ > "$PIDFILE"
+  # Acquire lock
+  flock -n $FD \
+    && return 0 \
+    || return 1
+}
 
-echo "===> Wormhole startup options"
-echo "Remote base:    ${BASE}"
-echo "Local base:     ${TRNS}"
-echo "Local port:     ${PORT}"
-echo "Local editor:   ${NET_VISUAL}"
-echo "Local terminal: ${NET_TERM}"
-echo
+eexit() {
+  local msg="$@"
 
-while [ true ]; do
-  echo -n "Listening @ "; date;
+  echo $msg >&2
+  exit 1
+}
 
-  (nc -p ${PORT} -l 127.0.0.1 || nc -l 127.0.0.1 ${PORT}) 2>/dev/null | while read -r -a 'RARG' ; do
-    echo -n "<<< @ "; date;
-    echo "<<< ${RARG[*]}"
+print_configuration() {
+  echo "===> Wormhole startup options"
 
-    COMMAND=${RARG[0]}
-    unset RARG[0]
+  echo "Remote base:    ${BASE}"
+  echo "Local base:     ${TRNS}"
+  echo "Local port:     ${PORT}"
+  echo "Local editor:   ${NET_VISUAL}"
+  echo "Local terminal: ${NET_TERM}"
+  echo
+}
 
-    if [ ${#RARG[@]} -lt 1 ]; then
-      echo "You need to give at least one argument to a command...";
-      continue;
-    fi
+main_loop() {
+  while [ true ]; do
+    echo -n "Listening @ "; date;
 
-    # Now translate to local filesystem path ...
-    FILES=()
-    for FILE in "${RARG[@]}"; do
+    (nc -p ${PORT} -l 127.0.0.1 || nc -l 127.0.0.1 ${PORT}) 2>/dev/null | while read -r -a 'RARG' ; do
+      echo -n "<<< @ "; date;
+      echo "<<< ${RARG[*]}"
 
-      FILES+=($(echo "$FILE" | sed -r "s|$BASE|$TRNS|g"));
+      COMMAND=${RARG[0]}
+      unset RARG[0]
+
+      if [ ${#RARG[@]} -lt 1 ]; then
+        echo "You need to give at least one argument to a command...";
+        continue;
+      fi
+
+      # Now translate to local filesystem path ...
+      FILES=()
+      for FILE in "${RARG[@]}"; do
+
+        FILES+=($(echo "$FILE" | sed -r "s|$BASE|$TRNS|g"));
+      done
+
+      case $COMMAND in
+        EDIT)
+
+          # Invoke editor
+          echo "EDIT > ${FILES[@]}"
+          "$NET_VISUAL" "${FILES[@]}" &
+          ;;
+
+        SHELL)
+          LOCATION="${FILES[0]}"
+          if [ ! -d "$LOCATION" ]; then
+            echo "Not a directory: $LOCATION";
+            continue;
+          fi
+
+          echo "SHELL > ${FILES[0]}"
+          cd $LOCATION && $NET_TERM &
+          ;;
+
+        EXPLORE)
+          LOCATION="${FILES[0]}"
+          if [ ! -d "$LOCATION" ]; then
+            echo "Not a directory: $LOCATION";
+            continue;
+          fi
+
+          echo "EXPLORER > $LOCATION"
+          cygstart $LOCATION &
+          ;;
+
+        START)
+          LOCATION="${FILES[0]}"
+          if [ ! -r "$LOCATION" ]; then
+            echo "Not a file: $LOCATION";
+            continue;
+          fi
+
+          echo "START > $LOCATION"
+          cygstart $LOCATION &
+          ;;
+
+        *)
+          echo "Unknown command $COMMAND";
+          ;;
+      esac
+
+      echo
     done
-
-    case $COMMAND in
-      EDIT)
-
-        # Invoke editor
-        echo "EDIT > ${FILES[@]}"
-        "$NET_VISUAL" "${FILES[@]}" &
-        ;;
-
-      SHELL)
-        LOCATION="${FILES[0]}"
-        if [ ! -d "$LOCATION" ]; then
-          echo "Not a directory: $LOCATION";
-          continue;
-        fi
-
-        echo "SHELL > ${FILES[0]}"
-        cd $LOCATION && $NET_TERM &
-        ;;
-
-      EXPLORE)
-        LOCATION="${FILES[0]}"
-        if [ ! -d "$LOCATION" ]; then
-          echo "Not a directory: $LOCATION";
-          continue;
-        fi
-
-        echo "EXPLORER > $LOCATION"
-        cygstart $LOCATION &
-        ;;
-
-      START)
-        LOCATION="${FILES[0]}"
-        if [ ! -r "$LOCATION" ]; then
-          echo "Not a file: $LOCATION";
-          continue;
-        fi
-
-        echo "START > $LOCATION"
-        cygstart $LOCATION &
-        ;;
-
-      *)
-        echo "Unknown command $COMMAND";
-        ;;
-    esac
-
-    echo
   done
-done
+}
+
+main() {
+  lock $PIDFILE || eexit "Cannot acquire lock, giving up."
+
+  print_configuration
+  main_loop
+}
+
+main
