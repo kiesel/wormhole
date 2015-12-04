52e35333da59c00bc73c9e794ee5fbe661b47981
Alex D'Andrea <alex.dandrea@1und1.de>
2015-12-04 20:21:23 +0100

    Split up into more fncts, improve output

--- host/bin/net-invoke.sh
+++ host/bin/net-invoke.sh
@@ -54,26 +54,26 @@ print_configuration() {
 
 main_loop() {
   while [ true ]; do
-    echo -n "Listening @ "; date;
+    echo -n "---> Listening @ "; date;
 
     (nc -p ${PORT} -l 127.0.0.1 || nc -l 127.0.0.1 ${PORT}) 2>/dev/null | while read -r -a 'RARG' ; do
-      echo -n "<<< @ "; date;
+      echo -n "<<< Line received @ "; date;
       echo "<<< ${RARG[*]}"
 
-      process_command $RARG
+      process_line "${RARG[@]}"
     done
   done
 }
 
-process_command() {
-  local ARGS="$1"
+process_line() {
+  local ARGS=("$@")
 
   COMMAND=${ARGS[0]}
   unset ARGS[0]
 
   if [ ${#ARGS[@]} -lt 1 ]; then
     echo "You need to give at least one argument to a command...";
-    continue;
+    return 1;
   fi
 
   # Now translate to local filesystem path ...
@@ -83,11 +83,18 @@ process_command() {
     FILES+=($(echo "$FILE" | sed -r "s|$BASE|$TRNS|g"));
   done
 
+  process_command $COMMAND "${FILES[@]}"
+  echo
+}
+
+process_command() {
+  local COMMAND=$1
+  local ARGS=("$@")
+
   case $COMMAND in
     EDIT)
 
       # Invoke editor
-      echo "EDIT > ${FILES[@]}"
       "$NET_VISUAL" "${FILES[@]}" &
       ;;
 
@@ -95,10 +102,9 @@ process_command() {
       LOCATION="${FILES[0]}"
       if [ ! -d "$LOCATION" ]; then
         echo "Not a directory: $LOCATION";
-        continue;
+        return 1;
       fi
 
-      echo "SHELL > ${FILES[0]}"
       cd $LOCATION && $NET_TERM &
       ;;
 
@@ -106,10 +112,9 @@ process_command() {
       LOCATION="${FILES[0]}"
       if [ ! -d "$LOCATION" ]; then
         echo "Not a directory: $LOCATION";
-        continue;
+        return 1;
       fi
 
-      echo "EXPLORER > $LOCATION"
       cygstart $LOCATION &
       ;;
 
@@ -117,20 +122,18 @@ process_command() {
       LOCATION="${FILES[0]}"
       if [ ! -r "$LOCATION" ]; then
         echo "Not a file: $LOCATION";
-        continue;
       fi
 
-      echo "START > $LOCATION"
       cygstart $LOCATION &
       ;;
 
     *)
       echo "Unknown command $COMMAND";
+      return 1;
       ;;
   esac
 
-  echo
-
+  return 0;
 }
 
 main() {
