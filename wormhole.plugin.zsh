3cc3e99a692362cf04eb593acc9bfab16f2b0ba9
Alex D'Andrea <alex.dandrea@1und1.de>
2016-04-11 16:29:21 +0200

    Move client code into main .zsh file

--- wormhole.plugin.zsh
+++ wormhole.plugin.zsh
@@ -1,15 +1,67 @@
+# Send given command via netcat
+#
+wormhole_send_command() {
+  if [ -z "$SSH_CLIENT" ]; then
+    echo "No SSH_CLIENT environment variable found. Cannot send wormhole command.";
+    return 1;
+  fi
 
-# Detect host or guest mode:
-if [ -n "${WORMHOLE_REMOTE}" -o \
-     -n "${WORMHOLE_LOCAL}" -o \
-     -n "${WORMHOLE_EDITOR}" -o \
-     -n "${WORMHOLE_TERM}" -o \
-     -n "${WORMHOLE_PORT}" ]; then
+  REMOTEIP=$(awk '{print $1;}' <<< $SSH_CLIENT)
+  echo "$@" | nc $REMOTEIP 5115 -q 0 -w 1
+}
 
-  nohup $(dirname $0)/host/bin/wormhole.sh </dev/null >> $HOME/wormhole.log 2>&1 &
-  disown
+expl() {
+  if [ $# -eq 0 ]; then
+    echo "expl <path>";
+    return 1;
+  fi
 
-else
+  PAYLOAD=$(realpath "$@")
+  wormhole_send_command "EXPLORE $PAYLOAD"
+}
 
-  source $(dirname $0)/guest/bin/wormhole-guest.sh
-fi
+start() {
+  if [ $# -eq 0 ]; then
+    echo "start <path>";
+    return 1;
+  fi
+
+  PAYLOAD=$(realpath "$@")
+  wormhole_send_command "START $PAYLOAD"
+}
+
+term() {
+  if [ $# -eq 0 ]; then
+    echo "term <path>";
+    return 1;
+  fi
+
+  PAYLOAD=$(realpath "$@")
+  wormhole_send_command "SHELL $PAYLOAD"
+}
+
+s() {
+  PAYLOAD=()
+  for f in "$@"; do
+
+    # If argument start with "-", treat is as option and keep unchanged
+    if [ '-' = ${f:0:1} ]; then
+      PAYLOAD+="$f"
+      continue;
+    fi
+
+    if [ -r "$f" ]; then
+      PAYLOAD+=($(realpath "$f"))
+    elif [ -d $(dirname "$f") ]; then
+      DIR=$(realpath $(dirname "$f"))
+      PAYLOAD+=($DIR/$(basename "$f"))
+    else
+      PAYLOAD+=($(realpath "$f"))
+    fi
+  done
+
+  wormhole_send_command "EDIT ${PAYLOAD[@]}"
+}
+
+# Publicly export functions
+export -f expl start term s >/dev/null
